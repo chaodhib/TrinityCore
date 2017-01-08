23a028a4e95a9854b34a3b8189e34c8b5a86c4bb
Chaouki Dhib <chaodhib@gmail.com>
2017-01-08 16:42:13 +0100

    fix hunter disengage. SPELL_EFFECT_LEAP_BACK was called twice. this commit should be reviewed and potentially rolled back if needed. Should not really be part of this PR.

---
+++ sql/updates/world/3.3.5/XXXX_XX_XX_XX_world_335.sql
@@ -0,0 +1,1 @@
+DELETE FROM `world`.`spell_linked_spell` WHERE `spell_trigger`='781';
--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -4759,8 +4759,7 @@ void Spell::EffectLeapBack(SpellEffIndex effIndex)
 
     float speedxy = m_spellInfo->Effects[effIndex].MiscValue / 10.f;
     float speedz = damage/ 10.f;
-    //1891: Disengage
-    unitTarget->JumpTo(speedxy, speedz, m_spellInfo->SpellIconID != 1891);
+    unitTarget->JumpTo(speedxy, speedz, false);
 }
 
 void Spell::EffectQuestClear(SpellEffIndex effIndex)
